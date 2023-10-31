db4edcc60ce79f176a36bd6b741bed85f78915c0
cristianmusica <75284701+cristianmusica@users.noreply.github.com>
2023-10-31 17:13:35 -0300

    Create Easy.sh

---
+++ Easy.sh
@@ -0,0 +1,7 @@
+sudo su
+cd
+git clone https://github.com/cristianmusica/VPS-Statistics-Web-better/
+cd VPS-Statistics-Web-better
+echo "Hagamosla simple, la instalare por ti de forma automatizada."
+chmod -R 777 install.sh
+./install.sh
